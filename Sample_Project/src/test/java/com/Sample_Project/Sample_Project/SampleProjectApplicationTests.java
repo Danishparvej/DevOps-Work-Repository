8e13b02f485fc2bdd6232605a4f718312291befa
danish <dsheikh176@gmail.com>
2024-02-09 10:48:20 +0530

    commited

---
+++ Sample_Project/src/test/java/com/Sample_Project/Sample_Project/SampleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sample_Project.Sample_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
